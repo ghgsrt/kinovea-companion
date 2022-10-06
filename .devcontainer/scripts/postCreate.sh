35c1ead5704ddb21f8f7e92d8c2bd071e5379ddb
ghgsrt <ajb7366@psu.edu>
2022-10-06 15:20:41 -0400

    ignore

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,5 +1,5 @@
 cd frontend \
-    && go install github.com/wailsapp/wails/v2/cmd/wails@latest \
+    && go get github.com/wailsapp/wails/v2/cmd/wails@latest \
     && npm i \
     && npm i --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint \
     && npm run build \

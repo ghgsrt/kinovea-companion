c0f04cb96fec05126c870b4d3ccc4095af37663e
ghgsrt <ajb7366@psu.edu>
2022-10-06 17:49:10 -0400

    ignore

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,6 +1,7 @@
 go install -v golang.org/x/tools/gopls@latest
 go install github.com/wailsapp/wails/v2/cmd/wails@latest
-cd frontend && npm i
+cd frontend
+npm i
 npm i --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint
 npm run build
 cd ../

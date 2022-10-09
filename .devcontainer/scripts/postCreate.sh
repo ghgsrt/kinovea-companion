07ebec60197dc960147ae41b68b441c8cf6037fd
ghgsrt <ajb7366@psu.edu>
2022-10-09 19:18:02 -0400

    ignore

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,1 +1,1 @@
-cd frontend && npm i && npm i --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint && npm run build && cd ../ && wails dev -s
+cd frontend && npm i && npm i --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint && npm run build && cd ../
